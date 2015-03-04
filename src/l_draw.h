7f3caf67616e2a23d805309313dfad2036bbbb64
Thomas Versteeg <thomasversteeg@gmx.com>
2015-03-04 22:51:20 +0100

    Finish draw library functions

--- src/l_draw.h
+++ src/l_draw.h
@@ -12,16 +12,20 @@ typedef struct {
 } texture_t;
 
 typedef struct {
-	char letters, start, letterwidth, letterheight;
-	unsigned int width, height;
+	char letters, start, width, height;
+	unsigned int totalwidth;
 	bool *pixels;
 } font_t;
 
 void initTexture(texture_t *tex, unsigned int width, unsigned int height);
 void initFont(font_t *font, unsigned int width, unsigned int height);
+void loadFont(font_t *font, char start, char letters, char width, const bool *pixels);
 
-inline bool getPixel(texture_t *tex, pixel_t *pixel, unsigned x, unsigned y);
-inline void setPixel(texture_t *tex, unsigned x, unsigned y, pixel_t pixel);
+inline bool getPixel(texture_t *tex, pixel_t *pixel, unsigned int x, unsigned int y);
+// Force setting the pixel in a unsafe way
+inline void setPixel(texture_t *tex, unsigned int x, unsigned int y, pixel_t pixel);
+// Set the pixel with a boundary check
+inline void drawPixel(texture_t *tex, int x, int y, pixel_t pixel);
 
 void drawLine(texture_t *tex, xy_t p1, xy_t p2, char r, char g, char b, char a);
 void drawCircle(xy_t p, unsigned int radius, char r, char g, char b, char a);
