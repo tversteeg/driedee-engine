eb03815287206d72645b07a8ec5e801f52169e55
Thomas Versteeg <thomasversteeg@gmx.com>
2015-03-08 11:21:13 +0100

    Add @jobtalle's pool implementation

--- src/l_sector.c
+++ src/l_sector.c
@@ -1,11 +1,17 @@
 #include "l_sector.h"
 
-sector_t *sectors = NULL;
-unsigned int nsectors = 0;
+static sector_t *sectors = NULL;
+static unsigned int nsectors = 0;
+static edge_t *edges = NULL;
+static unsigned int nedges = 0;
+static xy_t *vertices = NULL;
+static unsigned int nvertices = 0;
 
-unsigned int createSector()
+sector_t* createSector()
 {
 	sectors = (sector_t*)realloc(sectors, ++nsectors * sizeof(sector_t));
 
-	return nsectors;
+	return sectors + (nsectors - 1);
 }
+
+
---
+++ src/l_utils.h
@@ -0,0 +1,25 @@
+/* jobtalle's dynamic pool implementation */
+
+#pragma once
+
+#define POOL_BLOCKS  1024
+
+typedef struct poolFreed{
+	struct poolFreed *nextFree;
+} poolFreed;
+
+typedef struct {
+	unsigned int elementSize;
+	unsigned int blockSize;
+	unsigned int used;
+	int block;
+	poolFreed *freed;
+	char *blocks[POOL_BLOCKS];
+} pool;
+
+void poolInitialize(pool *p, unsigned int elementSize, unsigned int blockSize);
+void poolFreePool(pool *p);
+
+void *poolMalloc(pool *p);
+void poolFree(pool *p, void *ptr);
+void poolFreeAll(pool *p);
