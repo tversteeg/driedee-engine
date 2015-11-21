cd3e50fcbfa28daab07339b16b41cd90da695cc9
Thomas Versteeg <thomasversteeg@gmx.com>
2015-11-21 13:59:55 +0800

    Add start of separating map functions from r_main.c

---
+++ src/g_map.h
@@ -0,0 +1,16 @@
+#pragma once
+
+typedef char tile_t;
+
+typedef struct {
+	tile_t *tiles;
+	unsigned int size, width;
+} map_t;
+
+map_t* createMap(unsigned int width, unsigned int height);
+
+void setMapTileFromChar(map_t *map, unsigned int index, char type);
+
+unsigned int getMapX(map_t *map, unsigned int index);
+unsigned int getMapY(map_t *map, unsigned int index);
+unsigned int getMapIndex(map_t *map, unsigned int x, unsigned int y);
