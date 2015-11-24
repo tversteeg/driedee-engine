d521125e153a5b69bf293a97fbdda18a3b6016d6
Thomas Versteeg <thomasversteeg@gmx.com>
2015-11-25 00:03:33 +0800

    Create basic squares for each pixel on the map

--- src/g_level.c
+++ src/g_level.c
@@ -1,6 +1,20 @@
+#include "l_sector.h"
+
 #include "g_level.h"
 
 void createLevelFromMap(map_t *map)
 {
+	unsigned int i;
+	for(i = 0; i < map->size; i++){
+		xy_t vert = {getMapX(map, i), getMapY(map, i)};
+		edge_t edge = {.type = 0};
+		sector_t *sect = createSector(vert, &edge);
 
+		vert.x = 1;
+		createEdge(sect, vert, &edge);
+		vert.y = 1;
+		createEdge(sect, vert, &edge);
+		vert.x = 0;
+		createEdge(sect, vert, &edge);
+	}
 }
