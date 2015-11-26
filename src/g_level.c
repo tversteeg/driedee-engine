7323e1e2ebd5993887565239ddfeb991f2aa520e
Thomas Versteeg <thomasversteeg@gmx.com>
2015-11-26 18:57:20 +0800

    Cleanup function a bit

--- src/g_level.c
+++ src/g_level.c
@@ -4,52 +4,32 @@
 
 #include <stdio.h>
 
-int getEdgeType(map_t *map, unsigned int index, int offsetx, int offsety){
-	int x = getMapX(map, index);
-	int nextx = x + offsetx;
-	if(nextx < 0 || nextx > map->width){
-		return 1;
-	}
-	int y = getMapY(map, index);
-	int nexty = y + offsety;
-	if(nexty < 0 || nexty > map->size / map->width){
-		return 1;
-	}
-
-	if(map->tiles[getMapIndex(map, nextx, nexty)] == '.'){
-		return 0;
-	}
-
-	return 1;
-}
-
 void createLevelFromMap(map_t *map)
 {
-	unsigned int i, sectors = 0;
+	unsigned int i;
+	sector_t *lastsect = NULL;
 	for(i = 0; i < map->size; i++){
 		if(map->tiles[i] != '.'){
 			continue;
 		}
 		xy_t vert = {getMapX(map, i) * 32, getMapY(map, i) * 32};
-		edge_t edge;
+		edge_t edge = {.type = 1};
 		// Top
-		edge.type = getEdgeType(map, i, 0, -1);
 		sector_t *sect = createSector(vert, &edge);
 		// Right
 		vert.x += 32;
-		edge.type = getEdgeType(map, i, 1, 0);
-		createEdge(sect, vert, &edge);
+		createEdge(sect, vert, &edge)->texture = 1;
 		// Bottom
 		vert.y += 32;
-		edge.type = getEdgeType(map, i, 0, 1);
-		createEdge(sect, vert, &edge);
+		createEdge(sect, vert, &edge)->texture = 0;
 		// Left
 		vert.x -= 32;
-		edge.type = getEdgeType(map, i, -1, 0);
-		edge_t *edge1 = createEdge(sect, vert, &edge);
-		if(edge.type == 0){
-			printf("S: %d i: %d\n", sectors, i);
-			edge_t *edge2 = getSector(sectors)->edges + 1;
+		createEdge(sect, vert, &edge)->texture = 0;
+		if(i > 0 && i % map->width != 0 && map->tiles[i - 1] == '.'){
+			edge_t *edge1 = sect->edges + 3;
+			edge_t *edge2 = lastsect->edges + 1;
+
+			edge1->type = edge2->type = 0;
 			edge1->neighbor = edge2;
 			edge2->neighbor = edge1;
 		}
@@ -59,6 +39,6 @@ void createLevelFromMap(map_t *map)
 		sect->ceil = 10;
 		sect->floor = -10;
 
-		sectors++;
+		lastsect = sect;
 	}
 }
