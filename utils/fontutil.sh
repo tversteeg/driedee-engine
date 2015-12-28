b2427b8ece274e0a8e73ae5592d6d336e3e0adaa
Thomas Versteeg <thomasversteeg@gmx.com>
2015-12-28 18:12:47 +0700

    Font util: create PBM output file for easier parsing later

--- utils/fontutil.sh
+++ utils/fontutil.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]
+if [ $# -ne 2 ]
   then
-    echo "Pass the font name to convert as the first argument, and the output name as the second one"
+    echo "Pass the font name to convert as the first argument, and the output name as the second one" ;
+		exit 1
 fi
 
 fc-list | grep -q "$1" || { echo "$1 is not a valid font" ; exit 1 ; }
@@ -23,6 +24,8 @@ STRING=$(printf $(printf '\%o' {33..127}))
 convert -version > /dev/null || { echo "ImageMagick is not installed" ; exit 1 ; }
 convert -list font | grep -q $NAME || { echo "Can not find font in ImageMagick cache" ; exit 1 ; }
 
-echo -n "$STRING" | convert -background none -fill black \
+echo -n "$STRING" | convert -compress None \
 	-font "$FONT" -pointsize $HEIGHT -size $IMGWIDTH \
-	label:@- /tmp/output.gif
+	caption:@- /tmp/output.pbm
+
+sed -i '1d' /tmp/output.pbm
