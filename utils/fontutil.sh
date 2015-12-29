b4f775e63d6f4124a2ad02e0b379a8d24d73e0ed
Thomas Versteeg <thomasversteeg@gmx.com>
2015-12-29 10:44:00 +0700

    Font util: finish working version

--- utils/fontutil.sh
+++ utils/fontutil.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+convert -version > /dev/null || { echo "ImageMagick is not installed" ; exit 1 ; }
+
+if [ $# -ne 1 ]
   then
-    echo "Pass the font name to convert as the first argument, and the output name as the second one" ;
+    echo "Pass the font name to convert as the first argument" ;
 		exit 1
 fi
 
@@ -12,20 +14,53 @@ FONT=$(fc-list | grep "$1" | head -1 | sed 's/:.*//')
 
 QUERY=$(fc-query "$FONT")
 
-# echo "$QUERY"
+#echo "$QUERY"
 
 NAME=$(echo "$QUERY" | grep "family" | sed 's/.*"\(.*\)"[^"]*$/\1/')
-WIDTH=$(( $(echo "$QUERY" | grep "weight" | grep -Po '\d+') / 10 ))
-IMGWIDTH=$(((127 - 33) * $WIDTH))
 HEIGHT=$(echo "$QUERY" | grep "pixelsize" | grep -Po '\d+')
 
-STRING=$(printf $(printf '\%o' {33..127}))
-
-convert -version > /dev/null || { echo "ImageMagick is not installed" ; exit 1 ; }
 convert -list font | grep -q $NAME || { echo "Can not find font in ImageMagick cache" ; exit 1 ; }
 
+# Get the actual image width
+convert -compress None -font "$FONT" -pointsize $HEIGHT label:"A" /tmp/sizes.pbm
+sed -i '1d' /tmp/sizes.pbm
+
+# Extract size of the image
+TEXTSIZES=$(head -1 /tmp/sizes.pbm)
+SIZES=(${TEXTSIZES// / })
+
+FONTSTART=33
+FONTEND=127
+FONTRANGE=$(($FONTEND - $FONTSTART))
+
+IMGWIDTH=$(($FONTRANGE * (${SIZES[0]} - 2)))
+
+STRING=$(printf $(printf '\%o' {33..127}))
+# Create a image with the whole array of letters
 echo -n "$STRING" | convert -compress None \
-	-font "$FONT" -pointsize $HEIGHT -size $IMGWIDTH \
+	-font "$FONT" -pointsize $HEIGHT -size "${IMGWIDTH}x$HEIGHT" \
 	caption:@- /tmp/output.pbm
 
-sed -i '1d' /tmp/output.pbm
+# Remove header
+sed -i '1,2d' /tmp/output.pbm
+
+LOWERNAME=$(echo $NAME | tr '[:upper:]' '[:lower:]')
+OUTPUTFILENAME="pf_$LOWERNAME.h"
+
+# Generate C header file
+echo -e \	"// Generated font file
+static unsigned int ${LOWERNAME}fontwidth = $IMGWIDTH;
+static unsigned int ${LOWERNAME}fontheight = $HEIGHT;
+static char ${LOWERNAME}fontstart = $FONTSTART;
+static char ${LOWERNAME}fontrange = $FONTRANGE;
+static char ${LOWERNAME}fontdata[] = {
+	" > $OUTPUTFILENAME
+
+sed 's/ /,/g' /tmp/output.pbm >> $OUTPUTFILENAME
+
+echo -e "\n};" >> $OUTPUTFILENAME
+
+echo -e "Font header $NAME ($OUTPUTFILENAME) created\n\tglyph size: $((${SIZES[0]} - 2))x$HEIGHT\n\twidth: $IMGWIDTH"
+
+#rm /tmp/sizes.pbm
+#rm /tmp/output.pbm
